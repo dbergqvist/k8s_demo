8bfa1959f67af2e55139e9eab19f9ec0289df446
Ian Lewis <ianlewis@google.com>
2015-05-19 21:51:31 +0900

    Updated the sync.sh script for development

--- sync.sh
+++ sync.sh
@@ -1,18 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$@" == "" ]; then
+INSTANCE=$@
+
+if [ "$INSTANCE" == "" ]; then
     echo "Usage: sync.sh <instance-name>"
     exit 1
 fi
 
 echo "Getting project..."
-PROJECT=`gcloud config list | python config.py - core/project`
+PROJECT=`gcloud config list | python guestbook/config.py - core/project`
 echo "Getting zone..."
-ZONE=`gcloud config list | python config.py - compute/zone`
+ZONE=`gcloud config list | python guestbook/config.py - compute/zone`
 
 echo "Getting ssh config..."
 CONFIGFILE=`mktemp -t next-k8s-demo`
 gcloud compute config-ssh --ssh-config-file=$CONFIGFILE
 
 HOST=$@.$ZONE.$PROJECT
-rsync -e "ssh -F $CONFIGFILE" --progress -r guestbook/ $HOST:guestbook/
+rsync -a --delete-after -e "ssh -F $CONFIGFILE" --progress -r guestbook/ $HOST:guestbook/
